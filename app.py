94e5c1600ec30a3d4e71e53c022c88b8425d7fcb
urvigupta306 <urvigupta306@gmail.com>
2025-05-15 21:17:15 +0530

    app.py

--- app.py
+++ app.py
@@ -13,14 +13,13 @@ import pandas as pd
 
 if st.button("Predict"):
     input_dict = {
-        'step': [step],
-        'amount': [amount],
-        'oldbalanceOrg': [oldbalanceOrg],
-        'newbalanceOrig': [newbalanceOrig],
-        'oldbalanceDest': [oldbalanceDest],
-        'newbalanceDest': [newbalanceDest],
-        'TRANSFER': [transfer],
-        'CASH_OUT': [cash_out]
+        step = st.number_input("Step", min_value=0)
+amount = st.number_input("Transaction Amount", min_value=0.0)
+oldbalanceOrg = st.number_input("Old Balance (Origin)", min_value=0.0)
+newbalanceOrig = st.number_input("New Balance (Origin)", min_value=0.0)
+oldbalanceDest = st.number_input("Old Balance (Destination)", min_value=0.0)
+newbalanceDest = st.number_input("New Balance (Destination)", min_value=0.0)
+
     }
 
     input_df = pd.DataFrame(input_dict)
