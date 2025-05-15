df4e85015503c8718ddbd739be9ff90382eed1a2
urvigupta306 <urvigupta306@gmail.com>
2025-05-15 21:40:19 +0530

    app.py

--- app.py
+++ app.py
@@ -12,21 +12,25 @@ st.write("Enter transaction details to check if it's fraudulent.")
 
 step = st.number_input("Step (Time step)", min_value=0)
 amount = st.number_input("Transaction Amount", min_value=0.0)
-oldbalanceOrg = st.number_input("Old Balance (Origin Account)", min_value=0.0)
-newbalanceOrig = st.number_input("New Balance (Origin Account)", min_value=0.0)
-oldbalanceDest = st.number_input("Old Balance (Destination Account)", min_value=0.0)
-newbalanceDest = st.number_input("New Balance (Destination Account)", min_value=0.0)
-transaction_type = st.selectbox("Transaction Type", ["CASH_OUT", "TRANSFER", "Other"])
+oldbalanceOrg = st.number_input("Old Balance (Origin)", min_value=0.0)
+newbalanceOrig = st.number_input("New Balance (Origin)", min_value=0.0)
+oldbalanceDest = st.number_input("Old Balance (Destination)", min_value=0.0)
+newbalanceDest = st.number_input("New Balance (Destination)", min_value=0.0)
+isFlaggedFraud = st.selectbox("Is Flagged Fraud?", [0, 1])
+
+
+transaction_type = st.selectbox("Transaction Type", ["CASH_OUT", "DEBIT", "PAYMENT", "TRANSFER", "Other"])
 
 
 type_CASH_OUT = 1 if transaction_type == "CASH_OUT" else 0
+type_DEBIT = 1 if transaction_type == "DEBIT" else 0
+type_PAYMENT = 1 if transaction_type == "PAYMENT" else 0
 type_TRANSFER = 1 if transaction_type == "TRANSFER" else 0
-
 
 if st.button("Predict"):
     input_data = np.array([[step, amount, oldbalanceOrg, newbalanceOrig,
-                            oldbalanceDest, newbalanceDest,
-                            type_CASH_OUT, type_TRANSFER]])
+                            oldbalanceDest, newbalanceDest, isFlaggedFraud,
+                            type_CASH_OUT, type_DEBIT, type_PAYMENT, type_TRANSFER]])
 
     try:
         prediction = model.predict(input_data)[0]
